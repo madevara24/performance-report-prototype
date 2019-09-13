a24615f4692ded8d1d310e644d41513a44cb44d4
madevara24 <adityadevara91@gmail.com>
2019-09-14 00:29:04 +0700

    Add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="grey--text">
+          <span class="font-weight-light">AU</span>
+          <span class="">SOM</span>
+      </v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>person</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
