92e761beb294340b755bc7f0bb6158a352ac2e42
madevara24 <adityadevara91@gmail.com>
2019-09-13 23:33:12 +0700

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vuetify from 'vuetify';
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'md',
+  }
+})
+
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
