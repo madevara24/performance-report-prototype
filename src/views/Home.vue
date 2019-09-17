e500482692f53081cf95e6c98430f2cef2e7309f
madevara24 <adityadevara91@gmail.com>
2019-09-17 13:28:45 +0700

    Add project card component for dashboard Still need fixing on the card title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <v-container class="my-5">
+      <DashboardProjectCard v-for="project in projects" :key="project.name" :projectData="project"/>      
+    </v-container>
+  </div>
 </template>
 
 <script>
-
+import DashboardProjectCard from '@/components/DashboardProjectCard'
 export default {
   components: {
-    
+    DashboardProjectCard
+  },
+  data() {
+    return {
+      projects:[
+        {id:1, name: 'Insight', sprintDuration:7, sprintProgress:3, totalTasks:12, completedTasks:7},
+        {id:2, name: 'Hexagon', sprintDuration:14, sprintProgress:8, totalTasks:17, completedTasks:10}
+      ]
+    }
   },
 };
 </script>
