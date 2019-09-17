5de898cf3b4c55ad267f610d1f275131d6a6f0e1
madevara24 <adityadevara91@gmail.com>
2019-09-17 08:52:35 +0700

    Add navigation drawers

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-app-bar>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar flat app>
+      <v-app-bar-nav-icon @click="sideDrawer = !sideDrawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title class="grey--text">
           <span class="font-weight-light">AU</span>
@@ -36,11 +36,35 @@
         </v-list>
       </v-menu>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="sideDrawer" app>
+      <v-list>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="">{{ link.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="">{{link.text}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+    </v-navigation-drawer>
+
   </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+      return {
+        sideDrawer: false,
+        links:[
+                {icon: 'dashboard', text: 'Dashboard', route: '/' },
+                {icon: 'folder', text: 'Projects', route: '/projects' },
+                {icon: 'group', text: 'Teams', route: '/teams' },
+                {icon: 'person', text: 'Individual', route: '/individuals' },
+            ],
+      }
+    },
 }
 </script>
