1f973b04f9a583d40955fb34ff9e9787996fdc86
madevara24 <adityadevara91@gmail.com>
2019-11-11 10:17:10 +0700

    Add and customize chart view for task lifecycle and rejection

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -28,8 +28,11 @@
           <v-card flat class="px-3 mb-5">
             <v-card-title>Rejection</v-card-title>
             <v-card-text>
-              The rejection graph goes here
-              Rejection : average number of task iteration per iteration
+              <GChart
+                type="LineChart"
+                :data="rejection.chartData"
+                :options="rejection.chartOptions"
+              />
             </v-card-text>
           </v-card>
         </v-col>
@@ -37,8 +40,12 @@
           <v-card flat class="px-3 mb-5">
             <v-card-title>Task Lifecycle</v-card-title>
             <v-card-text>
-              The task lifecycle graph goes here
-              Task lifecycle : average duration of each task state (per iteration)
+              <GChart
+                style="height: 500px;"
+                type="PieChart"
+                :data="taskLifecycle.chartData"
+                :options="taskLifecycle.chartOptions"
+              />
             </v-card-text>
           </v-card>
         </v-col>
@@ -46,7 +53,11 @@
           <v-card flat class="px-3 mb-5">
             <v-card-title>Estimation Chart</v-card-title>
             <v-card-text>
-              
+              <!-- <GChart
+                type="Timeline"
+                :data="estimation.chartData"
+                :options="estimation.chartOptions"
+              /> -->
             </v-card-text>
           </v-card>
         </v-col>
@@ -78,10 +89,8 @@ export default {
           ['Sunday', 3, 0],
         ],
         chartOptions: {
-          chart: {
-            title: 'Burndown Chart',
-            subtitle: 'Points Remaining, Iteration : 2',
-          }
+          title: 'Burndown Chart',
+          subtitle: 'Points Remaining, Iteration : 2',
         }
       },
       deliverability:{
@@ -94,14 +103,58 @@ export default {
           ['Current Iteration', 90]
         ],
         chartOptions: {
-          chart: {
-            title: 'Burndown Chart',
-            subtitle: 'Points Remaining, Iteration : 2',
+          title: 'Burndown Chart',
+        }
+      },
+      rejection:{
+        chartData: [
+          ['Iteration', 'Rejection Rate'],
+          ['Iteration 1', 12],
+          ['Iteration 2', 8],
+          ['Iteration 3', 18],
+          ['Iteration 4', 14],
+          ['Current Iteration', 9]
+        ],
+        chartOptions: {
+          title: 'Rejection Rate',
+          colors: ['red'],
+          vAxis : {
+            maxValue : 100
           }
         }
-      }
+      },
+      taskLifecycle:{
+        chartData: [
+          ['State', 'Avg. Hours'],
+          ['Planned', 2],
+          ['Started', 6],
+          ['Finished', 0.5],
+          ['Accepted', 1],
+          ['Rejected', 1]
+        ],
+        chartOptions: {
+          title: 'Task Lifecycle',
+          pieHole: 0.4,
+          colors: ['#dbdbdb','#f08000','#203e64','#629200','#a71f39']
+        }
+      },
+      estimation: {
+        chartData: [
+          ['Day', 'Points Remaining', 'Ideal Burndown'],
+          ['Monday', 15, 15],
+          ['Tuesday', 13, 11.25],
+          ['Wednesday', 11, 7.5],
+          ['Thursday', 6, 3.75],
+          ['Friday', 3, 0],
+          ['Saturday', 3, 0],
+          ['Sunday', 3, 0],
+        ],
+        chartOptions: {
+          title: 'Burndown Chart',
+          subtitle: 'Points Remaining, Iteration : 2',
+        }
+      },
     }
   },
-  
 }
 </script>
